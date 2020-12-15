52756493b2cfb1af289c2d2c055bbeab4356c78b
JavierIslas <dimcairion@live.com>
2020-12-15 18:33:21 -0300

    Actividad 4 finalizada

--- my-drawer-ng/src/app/app.module.ts
+++ my-drawer-ng/src/app/app.module.ts
@@ -1,48 +1,28 @@
 import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
-import { NativeScriptModule } from "@nativescript/angular";
+import { NativeScriptModule, registerElement } from "@nativescript/angular";
 import { NativeScriptUISideDrawerModule } from "nativescript-ui-sidedrawer/angular";
-import { NoticiasServices } from "./domain/noticias.service";
-
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { ActionReducerMap , StoreModule as NgRxStoreModule } from "@ngrx/store";
-import {
-    intializeNoticias,
-    noticiasEffects,
-    NoticiasState,
-    reducerNoticias
-} from "./domain/noticias-state.model"
-import { EffectsModule } from "@ngrx/effects";
-export interface AppState{
-    noticias: NoticiasState;
-}
-
-const reducers: ActionReducerMap<AppState> ={
-    noticias: reducerNoticias
-};
-
-const reducersInitialState = {
-    noticias: intializeNoticias()
-}
+import { NoticiasService } from "./domain/noticias.service";
+import "nativescript-dom"
+import { ItemsService } from "./domain/items.service";
+import { MinLenDirective } from "./domain/minLen.validator";
 
 @NgModule({
+    providers: [NoticiasService, ItemsService],
     bootstrap: [
         AppComponent
     ],
     imports: [
         AppRoutingModule,
         NativeScriptModule,
-        NativeScriptUISideDrawerModule,
-        NgRxStoreModule.forRoot(reducers, {initialState: reducersInitialState}),
-        EffectsModule.forRoot([noticiasEffects])
+        NativeScriptUISideDrawerModule
     ],
     declarations: [
-        AppComponent
+        AppComponent,
     ],
     schemas: [
         NO_ERRORS_SCHEMA
-    ],
-    providers: [NoticiasServices]
+    ]
 })
-
 export class AppModule { }
--- my-drawer-ng/src/app/search/search.component.ts
+++ my-drawer-ng/src/app/search/search.component.ts
@@ -1,81 +1,60 @@
-
 import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
 import { RadSideDrawer } from "nativescript-ui-sidedrawer";
-import { Application, Color, View } from "@nativescript/core";
-import { NoticiasServices } from "../domain/noticias.service";
-import { RouterExtensions } from "@nativescript/angular";
-import * as Toast from "nativescript-toasts";
-import { AppState } from "../app.module";
-import { Store } from "@ngrx/store";
-import { Noticia, NuevaNoticiaAction } from "../domain/noticias-state.model";
+import { Application, Color,} from "@nativescript/core";
+import { NoticiasService } from "../domain/noticias.service";
+import { android } from "@nativescript/core/application";
+import { View } from "@nativescript/core/ui/core/view";
 @Component({
     selector: "Search",
-    templateUrl: "./search.component.html",
-  //  providers: [NoticiasServices]
+    templateUrl: "./search.component.html"
 })
 export class SearchComponent implements OnInit {
-  resultados: Array<string>;
-  textFieldValue: string;
-  @ViewChild("layout") layout: ElementRef;
+    resultados: Array<string>;
+    public c: string;
+    @ViewChild("layout") layout: ElementRef;
+    @ViewChild("logo") logo: ElementRef;
 
-    constructor(private noticias: NoticiasServices, 
-      private routerExtensions: RouterExtensions,
-      private store: Store<AppState>
-      ) {
-        // Use the component constructor to inject providers.
+    constructor(public noticias: NoticiasService) {
+        this.c = "";
     }
 
     ngOnInit(): void {
-      this.store.select((state)=>  state.noticias.sugerida)
-      .subscribe((data)=>{
-        const f = data;
-        if(f != null){
-          Toast.show({text: "Sugerimos leer: "+f.titulo, duration: Toast.DURATION.SHORT});
-        }
-      });
+        
+        this.noticias.agregar("HOLA");
+        this.noticias.agregar("HOLA 2");
+        this.noticias.agregar("HOLA 3");
+        this.cambio();
+        
     }
 
     onDrawerButtonTap(): void {
         const sideDrawer = <RadSideDrawer>Application.getRootView();
         sideDrawer.showDrawer();
+    };
+    cambio() {       //METODO QUE SOLO SE EJECUTA EN ANDROID
+        if (android) {
+            this.c="ANDROID"
+        }
     }
-    onButtonTap(){
-      this.noticias.agregar(this.textFieldValue).then((r: any)=>{
-        console.log("resultados buscar ahora"+JSON.stringify(r));
-        this.resultados  = r;      
-      });
-    }
-
-
-
     onItemTap(x): void {
-      console.dir(x);
-
-    //   this.routerExtensions.navigate(["/DetalleSearch"], {
-    //     transition: {
-    //         name: "fade"
-    //     }
-    // });
-
-       this.store.dispatch(new NuevaNoticiaAction(new Noticia(x.view.bindingContext)));
-
+        console.dir(x);
     }
-
-    
-    buscarAhora(s: string ) {
-
-      console.dir("Buscar ahora"+s);
-      this.noticias.buscar(s).then((r: any)=>{
-        console.log("resultados buscar ahora"+JSON.stringify(r));
-        this.resultados  = r;
-      },(e) => {
-        console.log("error buscar ahora"+e);
-        Toast.show({ text: "Error en la busqueda", duration: Toast.DURATION.SHORT});
-      });
-
-
-   
- 
-       
-  }
+    buscarAhora(s: string) {
+        this.resultados = this.noticias.buscar().filter((x) => x.indexOf(s) >= 0);
+        const layout = <View>this.layout.nativeElement;
+        const logo = <View>this.logo.nativeElement;
+        layout.animate({
+            backgroundColor: new Color("blue"),
+            duration: 300,
+            delay: 300
+        }).then(() => layout.animate({
+            backgroundColor: new Color("white"),
+            duration: 300,
+            delay: 300
+        }));
+        logo.animate({
+            rotate: 360,
+            duration: 3000
+        });
+    }
 }
